6a7062102dddb3e9cafe7a20e29304c126e90007
Kang Zhou <kazhou@microsoft.com>
2018-05-15 21:17:47 -0700

    flipAndInvertImage2
    flipAndInvertImage2

--- Leetcode/Array/Flipping an Image.cs
+++ Leetcode/Array/Flipping an Image.cs
@@ -24,5 +24,20 @@ namespace Leetcode.Array
             }
             return ret;
         }
+
+        public int[][] flipAndInvertImage2(int[][] A)
+        {
+            int C = A[0].Length;
+            foreach (var row in A)
+            {
+                for (int i = 0; i < (C + 1) / 2; ++i)
+                {
+                    int tmp = row[i] ^ 1;
+                    row[i] = row[C - 1 - i] ^ 1;
+                    row[C - 1 - i] = tmp;
+                }
+            }
+            return A;
+        }
     }
 }
