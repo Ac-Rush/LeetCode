ba6d5d213318dfbbcfb1eedd875d8eb9af095131
Kang Zhou <kazhou@microsoft.com>
2018-05-16 10:37:49 -0700

    RemoveDuplicates2
    RemoveDuplicates2

--- Leetcode/Array/RemoveDuplicates.cs
+++ Leetcode/Array/RemoveDuplicates.cs
@@ -33,5 +33,21 @@ namespace Leetcode.Array
            // nums = list.ToArray();
             return len;
         }
+
+        //two index
+        public int RemoveDuplicates2(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
     }
 }
