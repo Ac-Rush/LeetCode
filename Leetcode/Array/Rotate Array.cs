43d86aa7c91a1fe37f94760839b8e51ba5d116cc
Kang Zhou <kazhou@microsoft.com>
2018-05-15 21:33:00 -0700

    Rotate_Array
    Rotate_Array

---
+++ Leetcode/Array/Rotate Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    class Rotate_Array
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        private void Reverse(int[] nums, int l, int r)
+        {
+            while (l < r)
+            {
+                var temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r--;
+            }
+        }
+    }
+}
