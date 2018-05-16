fa05a234b19c9157dacd0bcdea5609f76d73358d
Kang Zhou <kazhou@microsoft.com>
2018-05-16 10:35:53 -0700

    RemoveDuplicates
    RemoveDuplicates

---
+++ Leetcode/Array/RemoveDuplicates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    class RemoveDuplicatesC
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            var pre = nums[0];
+            var len = 1;
+            var list = new List<int> { pre };
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != pre)
+                {
+                    len++;
+                    pre = nums[i];
+                    list.Add(pre);
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                nums[i] = list[i];
+            }
+           // nums = list.ToArray();
+            return len;
+        }
+    }
+}
