86d471f7b6dec1140bf768dcc93a85b94fe11a53
Kang Zhou <kazhou@microsoft.com>
2018-05-20 16:15:06 -0700

    ArrangeCoins
    ArrangeCoins

---
+++ Leetcode/BinarySearch/IsPerfectSquareC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class IsPerfectSquareC
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num == 0)
+            {
+                return true;
+            }
+            var sqart = (int)Math.Sqrt(num);
+            return sqart * sqart == num ? true : false;
+        }
+
+        public bool IsPerfectSquare2(int num)
+        {
+            if (num < 2)
+                return true;
+            long start, mid, midsq, end;
+            start = 1;
+            end = num / 2;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                midsq = mid * mid;
+                if (midsq == num)
+                {
+                    return true;
+                }
+                else if (midsq < num)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return false;
+        }
+    }
+}
