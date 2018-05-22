1c5f3cfdca29b43ba617d26d26e66f7c99aebed3
Kang Zhou <kazhou@microsoft.com>
2018-05-21 20:56:06 -0700

    Search_a_2D_Matrix
    Search_a_2D_Matrix

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -14,7 +14,7 @@ namespace Leetcode
     {
         static void Main(string[] args)
         {
-            var a = Is_Subsequence.IsSubsequence2("abc", "ahbgdc");
+            var a = Search_a_2D_Matrix.SearchMatrix(new int[,] { {1,3,5,7} , {10,11,16,20}, {23,30,34,50} }, 13);
             Console.WriteLine(a);
         }
     }
