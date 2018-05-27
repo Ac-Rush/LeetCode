0c24de194698466909136f640c1423df274f03d1
Kang Zhou <kazhou@microsoft.com>
2018-05-27 13:41:29 -0700

    Second_Minimum_Node_In_a_Binary_Tree
    Second_Minimum_Node_In_a_Binary_Tree

---
+++ Leetcode/Tree/Second Minimum Node In a Binary Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Second_Minimum_Node_In_a_Binary_Tree
+    {
+        private static int? min;
+        private static int? second;
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            if (root == null)
+            {
+                return -1;
+            }
+            FindSecondMinimumValue(root.left);
+            if (min == null)
+            {
+                min = root.val;
+            }
+            else
+            {
+                if (root.val < min)
+                {
+                    second = min;
+                    min = root.val;
+                }
+                else if (root.val > min && (second == null || (second != null && second > root.val)))
+                {
+                    second = root.val;
+                }
+            }
+            FindSecondMinimumValue(root.right);
+            return second ?? -1;
+        }
+    }
+}
