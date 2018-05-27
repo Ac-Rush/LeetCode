0a52a295decb024ec566702759bd2a5e5ab3edb1
Kang Zhou <kazhou@microsoft.com>
2018-05-27 14:43:37 -0700

    Path_Sum_III
    Path_Sum_III

---
+++ Leetcode/Tree/Path Sum III.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Path_Sum_III
+    {
+        public int PathSum(TreeNode root, int sum)
+        {
+            if (root == null) return 0;
+            return pathSumFrom(root, sum) + PathSum(root.left, sum) + PathSum(root.right, sum);
+        }
+
+        /// <summary>
+        /// 从根节点开始的path的个数
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        private int pathSumFrom(TreeNode node, int sum)
+        {
+            if (node == null) return 0;
+            return (node.val == sum ? 1 : 0)
+                + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
+        }
+    }
+}
