804bd5ec7a9cad20de6f784e4ab19a6313ee1155
justboy1987 <175636422@qq.com>
2018-05-30 11:37:20 +0800

    update

---
+++ Leetcode/Array/Contains Duplicate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    class Contains_Duplicate
+    {
+        /// <summary>
+        /// O（n^2）
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool containsDuplicate(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    if (nums[j] == nums[i]) return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// O（NlogN）
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool containsDuplicate2(int[] nums)
+        {
+            System.Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// HashSet O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool containsDuplicate3(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (int x in nums)
+            {
+                if (set.Contains(x)) return true;
+                set.Add(x);
+            }
+            return false;
+        }
+    }
+}
---
+++ Leetcode/Array/One1-bit and 2-bit Characters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    class One1_bit_and_2_bit_Characters
+    {
+        public bool IsOneBitCharacter(int[] bits)
+        {
+            int i = 0;
+            while (i < bits.Length - 1)
+            {
+                i += bits[i] + 1;
+            }
+            return i == bits.Length - 1;
+        }
+
+        /// <summary>
+        /// 贪心
+        /// </summary>
+        /// <param name="bits"></param>
+        /// <returns></returns>
+        public bool isOneBitCharacter2(int[] bits)
+        {
+            int i = bits.Length - 2;
+            while (i >= 0 && bits[i] > 0) i--;
+            return (bits.Length - i) % 2 == 0;
+        }
+    }
+}
