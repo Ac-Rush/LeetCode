33b1fa7f0e6e48020702ac17c50847b204d90776
Kang Zhou <kazhou@microsoft.com>
2018-05-30 16:25:49 -0700

    PreorderTraversal2
    PreorderTraversal2

--- Leetcode/Tree/Binary Tree Preorder Traversal.cs
+++ Leetcode/Tree/Binary Tree Preorder Traversal.cs
@@ -26,5 +26,33 @@ namespace Leetcode.Tree
             Preorder(root.left, result);
             Preorder(root.right, result);
         }
+
+        public IList<int> PreorderTraversal2(TreeNode root)
+        {
+
+            var result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+            var stack = new Stack<TreeNode>();
+            
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                result.Add(node.val);
+                
+                if (node.right != null)
+                {
+                    stack.Push(node.right);
+                }
+                if (node.left != null)
+                {
+                    stack.Push(node.left);
+                }
+            }
+            return result;
+        }
     }
 }
