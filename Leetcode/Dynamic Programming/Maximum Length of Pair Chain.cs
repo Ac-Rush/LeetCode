ae18e4a01959689d9937edfea5a6818dccfcea79
Kang Zhou <kazhou@microsoft.com>
2018-06-02 15:05:46 -0700

    FindLongestChain
    FindLongestChain

--- Leetcode/Dynamic Programming/Maximum Length of Pair Chain.cs
+++ Leetcode/Dynamic Programming/Maximum Length of Pair Chain.cs
@@ -30,45 +30,120 @@ The number of given pairs will be in the range [1, 1000].
 
         public int FindLongestChain(int[,] pairs)
         {
-            /*
-            var res = pairs;
+            pairs = pairs.OrderBy(row => row[0]);
             int N = pairs.GetLength(0);
             int[] dp = new int[N];
-            for (var index = 0; index < dp.Length; index++)
+            var max = 0;
+            for (int i = 0; i < dp.Length; i++)
             {
-                dp[index] = 1;
+                dp[i] = 1;
             }
 
             for (int j = 1; j < N; ++j)
             {
                 for (int i = 0; i < j; ++i)
                 {
-                    if (res[i, 1] < res[j, 0])
+                    if (pairs[i, 1] < pairs[j, 0])
+                    {
                         dp[j] = Math.Max(dp[j], dp[i] + 1);
+                    }
                 }
+
+                max = Math.Max(max, dp[j]);
             }
 
-            return dp.Max();
+            return max;
+        }
 
-    */
 
-            System.Array.Sort<int[]>(pairs, (a, b) => a[0] - b[0]);
-            int N = pairs.length;
-            int[] dp = new int[N];
-            Arrays.fill(dp, 1);
-
-            for (int j = 1; j < N; ++j)
+        /// <summary>
+        /// 贪心， 找第二个最小的
+        /// </summary>
+        /// <param name="pairs"></param>
+        /// <returns></returns>
+        public int findLongestChain2(int[,] pairs)
+        {
+            pairs = pairs.OrderBy(row => row[1]);
+            int cur = int.MinValue, ans = 0;
+            for (int i = 0; i < pairs.GetLength(0); i++)
             {
-                for (int i = 0; i < j; ++i)
+                if (cur < pairs[i,0])
                 {
-                    if (pairs[i][1] < pairs[j][0])
-                        dp[j] = Math.max(dp[j], dp[i] + 1);
+                    cur = pairs[i, 1];
+                    ans++;
                 }
             }
-
-            int ans = 0;
-            for (int x: dp) if (x > ans) ans = x;
             return ans;
+        }
+    }
+
+
+    public static class MultiDimensionalArrayExtensions
+    {
+        /// <summary>
+        ///   Orders the two dimensional array by the provided key in the key selector.
+        /// </summary>
+        /// <typeparam name="T">The type of the source two-dimensional array.</typeparam>
+        /// <param name="source">The source two-dimensional array.</param>
+        /// <param name="keySelector">The selector to retrieve the column to sort on.</param>
+        /// <returns>A new two dimensional array sorted on the key.</returns>
+        public static T[,] OrderBy<T>(this T[,] source, Func<T[], T> keySelector)
+        {
+            return source.ConvertToSingleDimension().OrderBy(keySelector).ConvertToMultiDimensional();
+        }
+        /// <summary>
+        ///   Orders the two dimensional array by the provided key in the key selector in descending order.
+        /// </summary>
+        /// <typeparam name="T">The type of the source two-dimensional array.</typeparam>
+        /// <param name="source">The source two-dimensional array.</param>
+        /// <param name="keySelector">The selector to retrieve the column to sort on.</param>
+        /// <returns>A new two dimensional array sorted on the key.</returns>
+        public static T[,] OrderByDescending<T>(this T[,] source, Func<T[], T> keySelector)
+        {
+            return source.ConvertToSingleDimension().
+                OrderByDescending(keySelector).ConvertToMultiDimensional();
+        }
+        /// <summary>
+        ///   Converts a two dimensional array to single dimensional array.
+        /// </summary>
+        /// <typeparam name="T">The type of the two dimensional array.</typeparam>
+        /// <param name="source">The source two dimensional array.</param>
+        /// <returns>The repackaged two dimensional array as a single dimension based on rows.</returns>
+        private static IEnumerable<T[]> ConvertToSingleDimension<T>(this T[,] source)
+        {
+            T[] arRow;
+
+            for (int row = 0; row < source.GetLength(0); ++row)
+            {
+                arRow = new T[source.GetLength(1)];
+
+                for (int col = 0; col < source.GetLength(1); ++col)
+                    arRow[col] = source[row, col];
+
+                yield return arRow;
+            }
+        }
+        /// <summary>
+        ///   Converts a collection of rows from a two dimensional array back into a two dimensional array.
+        /// </summary>
+        /// <typeparam name="T">The type of the two dimensional array.</typeparam>
+        /// <param name="source">The source collection of rows to convert.</param>
+        /// <returns>The two dimensional array.</returns>
+        private static T[,] ConvertToMultiDimensional<T>(this IEnumerable<T[]> source)
+        {
+            T[,] twoDimensional;
+            T[][] arrayOfArray;
+            int numberofColumns;
+
+            arrayOfArray = source.ToArray();
+            numberofColumns = (arrayOfArray.Length > 0) ? arrayOfArray[0].Length : 0;
+            twoDimensional = new T[arrayOfArray.Length, numberofColumns];
+
+            for (int row = 0; row < arrayOfArray.GetLength(0); ++row)
+                for (int col = 0; col < numberofColumns; ++col)
+                    twoDimensional[row, col] = arrayOfArray[row][col];
+
+            return twoDimensional;
         }
     }
 
