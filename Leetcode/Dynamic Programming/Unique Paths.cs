66add9f857217cd5bb0f454ccad95a8b78675276
Kang Zhou <kazhou@microsoft.com>
2018-06-03 16:25:32 -0700

    Unique_Paths
    Unique_Paths

---
+++ Leetcode/Dynamic Programming/Unique Paths.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    class Unique_Paths
+    {
+        /// <summary>
+        /// my solution
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int UniquePaths(int m, int n)
+        {
+            var dp = new int[m,n];
+            for (int i = 0; i < m; i++)
+            {
+                dp[i, 0] = 1;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dp[0,i] = 1;
+            }
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+            return dp[m - 1, n - 1];
+        }
+    }
+}
