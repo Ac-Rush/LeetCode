9b6b0977cc34af612c2b6be392a3b98e54c085bd
Kang Zhou <kazhou@microsoft.com>
2018-06-03 16:53:37 -0700

    class House_Robber
        class House_Robber

---
+++ Leetcode/Dynamic Programming/House Robber II.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    class House_Robber_II
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            return Math.Max(nums[0] + Rob(nums, 2, nums.Length - 2), Rob(nums, 1, nums.Length - 1));
+        }
+
+        public int Rob(int[] nums, int start, int end)
+        {
+            int rob = 0, preRob = 0;
+            for (int i = start; i <= end; i++)
+            {
+                var temp = rob;
+                rob = Math.Max(rob, preRob + nums[i]);
+                preRob = temp;
+            }
+            return rob;
+        }
+    }
+}
