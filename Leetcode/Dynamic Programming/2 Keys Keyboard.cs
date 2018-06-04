db6afb63ff4a63e015375df9180b8d46d9c2ff85
Kang Zhou <kazhou@microsoft.com>
2018-06-03 21:23:12 -0700

    _2_Keys_Keyboard
    _2_Keys_Keyboard

---
+++ Leetcode/Dynamic Programming/2 Keys Keyboard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    class _2_Keys_Keyboard
+    {
+        /// <summary>
+        /// 素数分解问题
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int MinSteps(int n)
+        {
+            int ans = 0, d = 2;
+            while (n > 1)
+            {
+                while (n % d == 0)
+                {
+                    ans += d;
+                    n /= d;
+                }
+                d++;
+            }
+            return ans;
+        }
+        private int FindGCD(int number1, int number2)
+        {
+            //base case
+            if (number2 == 0)
+            {
+                return number1;
+            }
+            return FindGCD(number2, number1 % number2);
+        }
+    }
+
+    
+    }
