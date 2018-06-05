1bf86bf4d6817b7086f5dd55634d3d2f52362c20
Kang Zhou <kazhou@microsoft.com>
2018-06-04 21:19:54 -0700

    Combination_Sum_II
    Combination_Sum_II

---
+++ Leetcode/DFS_BFS/Combination Sum II.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.DFS_BFS
+{
+    class Combination_Sum_II
+    {
+        IList<IList<int>> lists = new List<IList<int>>();
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            var list = new List<int>();
+            System.Array.Sort(candidates);
+            Helper(candidates, 0, target, list);
+            return lists;
+        }
+
+        public void Helper(int[] candidates, int index, int target, List<int> result)
+        {
+            if (target == 0)
+            {
+                lists.Add(new List<int>(result));
+            }
+            if (target > 0)
+            {
+                for (int i = index; i < candidates.Length; i++)
+                {
+                    if (i > index && candidates[i] == candidates[i - 1])
+                    {
+                        continue;
+                    }
+                    if (candidates[i] <= target)
+                    {
+                        result.Add(candidates[i]);
+                        Helper(candidates, i + 1, target - candidates[i], result);
+                        result.RemoveAt(result.Count - 1);
+                    }
+                }
+            }
+        }
+    }
+}
