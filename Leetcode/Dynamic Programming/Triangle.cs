809e8c539b15d1c6ed86ed5a9031e78f1f44cd0f
Kang Zhou <kazhou@microsoft.com>
2018-06-06 21:02:37 -0700

    Triangle
    Triangle

---
+++ Leetcode/Dynamic Programming/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    class Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            for (int i = triangle.Count -2; i >=0; i--)
+            {
+                for (int j = 0; j < triangle[i].Count; j++)
+                {
+                    triangle[i][j] = triangle[i][j] + Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                }
+            }
+            return triangle[0][0];
+        }
+    }
+}
