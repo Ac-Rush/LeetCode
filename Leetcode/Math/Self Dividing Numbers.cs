65f81cfc77e7d05d79056ba8d5e91c3979ad9699
Kang Zhou <kazhou@microsoft.com>
2018-06-06 22:12:26 -0700

    Self_Dividing_Numbers
    Self_Dividing_Numbers

---
+++ Leetcode/Math/Self Dividing Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class Self_Dividing_Numbers
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            var result = new List<int>();
+            for (int i = left; i <= right; i++)
+            {
+                if (IsSelfDividingNumber(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+
+        private bool IsSelfDividingNumber(int m)
+        {
+            var n = m;
+            while (n > 1)
+            {
+                var div = n % 10;
+                if (div == 0)
+                {
+                    return false;
+                }
+                if (m % div != 0)
+                {
+                    return false;
+                }
+                n = n / 10;
+            }
+            return true;
+        }
+    }
+}
