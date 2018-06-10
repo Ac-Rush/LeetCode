38df20d84b3728cc01f9ae7a37bff9041a473089
Kang Zhou <kazhou@microsoft.com>
2018-06-10 16:30:53 -0700

    Convert_Sorted_List_to_Binary_Search_Tree
    Convert_Sorted_List_to_Binary_Search_Tree

---
+++ Leetcode/LinkList/Odd Even Linked List.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Odd_Even_Linked_List
+    {
+        /// <summary>
+        /// my solution
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode OddEvenList(ListNode head)
+        {
+            var dummyOdd = new ListNode(0);
+            var dummyEven = new ListNode(0);
+            var oddHead = dummyOdd;
+            var evenHead = dummyEven;
+            var count = 1;
+            while (head != null)
+            {
+                if (count % 2 == 0)
+                {
+                    dummyEven.next = head;
+                    dummyEven = head;
+                }
+                else
+                {
+                    dummyOdd.next = head;
+                    dummyOdd = head;
+                }
+                head = head.next;
+                count++;
+            }
+            dummyOdd.next = evenHead.next;
+            dummyEven.next = null;
+            return oddHead.next;
+        }
+
+        public ListNode oddEvenList(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode odd = head, even = head.next, evenHead = even;
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
---
+++ Leetcode/LinkList/Swap Nodes in Pairs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Swap_Nodes_in_Pairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head?.next == null)
+                return head;
+            ListNode n = head.next;
+            head.next = SwapPairs(head.next.next);
+            n.next = head;
+            return n;
+        }
+    }
+}
