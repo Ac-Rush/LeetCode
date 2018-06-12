2f3ddf15a0bf12911066ff1973661dfbdb09da37
Kang Zhou <kazhou@microsoft.com>
2018-06-11 21:49:08 -0700

    FindAnagrams2
    FindAnagrams2

---
+++ Leetcode/HashTable/Isomorphic Strings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.HashTable
+{
+    class Isomorphic_Strings
+    {
+        /// <summary>
+        /// 太厉害了
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool IsIsomorphic(string s, string t)
+        {
+            var m1 = new int[256]; var m2 = new int[256];
+            int n = s.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                if (m1[s[i]] != m2[t[i]]) return false;
+                m1[s[i]] = i + 1;
+                m2[t[i]] = i + 1;
+            }
+            return true;
+        }
+    }
+}
---
+++ Leetcode/HashTable/Longest Palindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.HashTable
+{
+    class Longest_Palindrome
+    {
+        /// <summary>
+        /// my solution
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LongestPalindrome(string s)
+        {
+            var set = new int[58];
+            foreach (var c in s)
+            {
+                set[c - 'A']++;
+            }
+            var middle = false;
+            var count =0;
+            foreach (var c in set)
+            {
+                if (c%2 == 0)
+                {
+                    count += c;
+                }
+                else
+                {
+                    if (middle == false)
+                    {
+                        count += 1;
+                        middle = true;
+                    }
+                    count += c - 1;
+                }
+            }
+            return count;
+        }
+    }
+}
