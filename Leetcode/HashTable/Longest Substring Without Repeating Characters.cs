2be18e054cbf61f1c0c5bc4a0670071c6ab90cce
Kang Zhou <kazhou@microsoft.com>
2018-06-11 22:23:36 -0700

    Longest_Substring_Without_Repeating_Characters
    Longest_Substring_Without_Repeating_Characters

---
+++ Leetcode/HashTable/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.HashTable
+{
+    class Longest_Substring_Without_Repeating_Characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s == null || s.Length == 0 ) return 0;
+            int[] hash = new int[256]; //character hash
+            
+
+            int start = 0, end = 0,  max = 0;
+            while (end < s.Length)
+            {
+                if (hash[s[end]] == 0)
+                {
+                    hash[s[end++]]++;
+                    max = Math.Max(max, end - start);
+                }
+                else
+                {
+                    hash[s[start++]] = 0;
+                }
+            }
+            return max;
+        }
+    }
+}
