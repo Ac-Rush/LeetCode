bf858f715f7a283892aeae6f54af7ab6654f72e7
Kang Zhou <kazhou@microsoft.com>
2018-06-12 14:47:01 -0700

    Valid_Parentheses
    Valid_Parentheses

---
+++ Leetcode/Stack/Valid Parentheses.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Leetcode.Stack
+{
+    class Valid_Parentheses
+    {
+        
+        public bool IsValid(string s)
+        {
+
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(')
+                    stack.Push(')');
+                else if (c == '{')
+                    stack.Push('}');
+                else if (c == '[')
+                    stack.Push(']');
+                else if (stack.Count ==0 || stack.Pop() != c)
+                    return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
