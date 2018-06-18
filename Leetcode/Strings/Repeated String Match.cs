3e00fbdcb6803134941fe9d9c5dc3f32b6f7ead4
Kang Zhou <kazhou@microsoft.com>
2018-06-17 20:46:01 -0700

    Repeated_String_Match
    Repeated_String_Match

---
+++ Leetcode/Strings/Repeated String Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    class Repeated_String_Match
+    {
+        public int RepeatedStringMatch(string A, string B)
+        {
+            for (int i = 0, j = 0; i < A.Length; ++i)
+            {
+                for (j = 0; j < B.Length && A[(i + j) % A.Length] == B[j]; ++j) ;
+                if (j == B.Length) return (i + j) / A.Length + ((i + j) % A.Length != 0 ? 1 : 0);
+            }
+            return -1;
+        }
+    }
+}
