858bb74d33079e899e9b47ef4f6326316cb0d83b
Kang Zhou <kazhou@microsoft.com>
2018-06-17 18:16:58 -0700

    Repeated_Substring_Pattern
    Repeated_Substring_Pattern

---
+++ Leetcode/Strings/Reverse String II.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    class Reverse_String_II
+    {
+        public string ReverseStr(string s, int k)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < s.Length;)
+            {
+                var len = Math.Min(k, s.Length - i);
+                sb.Append(s.Substring(i, len).Reverse().ToArray()); // my bug was   sb.Append(s.Substring(i, len).Reverse());
+                i += len;
+                len = Math.Min(k, s.Length - i);
+                sb.Append(s.Substring(i, len));
+                i += len;
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Leetcode/Strings/Reverse Vowels of a String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    class Reverse_Vowels_of_a_String
+    {
+        public string ReverseVowels(string s)
+        {
+            var array = s.ToCharArray();
+            var set = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' }; //my bug 少了大写 要和面试官确认一下
+            var start = 0;
+            var end = s.Length - 1;
+            while (start < end)
+            {
+                while (start < end && !set.Contains(array[start])) start++;
+                while (start < end && !set.Contains(array[end])) end--;
+                var temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;  //my bug  忘了 start++ end --
+                end--;
+            }
+            return new string(array); // my bug string不能直接写（readonly） 需要先弄到 char[]里
+        }
+    }
+}
