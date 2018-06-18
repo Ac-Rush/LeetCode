25a83fcc8cf2d5e6e2bcc23183597e3ac906180b
Kang Zhou <kazhou@microsoft.com>
2018-06-17 18:32:11 -0700

    Number_of_Segments_in_a_String
    Number_of_Segments_in_a_String

---
+++ Leetcode/Strings/Count and Say.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    class Count_and_Say
+    {
+        public string CountAndSay(int n)
+        {
+            StringBuilder curr = new StringBuilder("1");
+            StringBuilder prev;
+            int count;
+            char say;
+            for (int i = 1; i < n; i++)
+            {
+                prev = curr;
+                curr = new StringBuilder();
+                count = 1;
+                say = prev[0];
+
+                for (int j = 1, len = prev.Length; j < len; j++)
+                {
+                    if (prev[j] != say)
+                    {
+                        curr.Append(count).Append(say);
+                        count = 1;
+                        say = prev[j];
+                    }
+                    else count++;
+                }
+                curr.Append(count).Append(say);
+            }
+            return curr.ToString();
+        }
+    }
+}
---
+++ Leetcode/Strings/Number of Segments in a String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Strings
+{
+    class Number_of_Segments_in_a_String
+    {
+        public int CountSegments(string s)
+        {
+            
+            return s.Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries).Length ;
+        }
+    }
+}
