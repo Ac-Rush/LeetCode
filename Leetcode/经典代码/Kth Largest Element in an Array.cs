04eb55f914c2a043bbd7bb0263b4cd17237b3ae8
Kang Zhou <kazhou@microsoft.com>
2018-06-22 09:57:59 -0700

    Wiggle_Sort_II
    Wiggle_Sort_II

---
+++ Leetcode/经典代码/Kth Largest Element in an Array.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.DAC
+{
+    class Kth_Largest_Element_in_an_Array
+    {
+        public int FindKthLargest(int[] a, int k)
+        {
+            int n = a.Length;
+            int p = quickSelect(a, 0, n - 1, n - k + 1);
+            return a[p];
+        }
+        public int quickSelect(int[] a, int lo, int hi, int k)
+        {
+            var p = Partition(a, lo, hi);
+            int m = p - lo + 1;
+
+            // pivot is the one!
+            if (m == k) return p;
+            // pivot is too big, so it must be on the left
+            else if (m > k) return quickSelect(a, lo, p - 1, k);
+            // pivot is too small, so it must be on the right
+            else return quickSelect(a, p + 1, hi, k - m);
+        }
+
+        private int Partition(int[] a, int lo, int hi)
+        {
+            int i = lo, j = hi, pivot = a[hi];
+            while (i < j)
+            {
+                if (a[i++] > pivot) swap(a, --i, --j);
+            }
+            swap(a, i, hi);
+
+            // count the nums that are <= pivot from lo
+            
+            return i;
+        }
+        void swap(int[] a, int i, int j)
+        {
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    }
+}
