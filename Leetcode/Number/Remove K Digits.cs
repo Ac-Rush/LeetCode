3bb5a263971a3e20addde8be4be9088964834c7f
Kang Zhou <kazhou@microsoft.com>
2018-06-23 14:38:12 -0700

    Remove_K_Digits
    Remove_K_Digits

--- Leetcode/Number/Remove K Digits.cs
+++ Leetcode/Number/Remove K Digits.cs
@@ -10,35 +10,29 @@ namespace Leetcode.Number
     public class Remove_K_Digits
     {
 
-        public string RemoveKdigitsMy2(string num, int k)
+        public string RemoveKdigits(string num, int k)
         {
-            var dp = new string[k + 1, num.Length + 1];
-            for (int i = 0; i < k + 1; i++)
+            int digits = num.Length - k;
+            char[] stk = new char[num.Length];
+            int top = 0;
+            // k keeps track of how many characters we can remove
+            // if the previous character in stk is larger than the current one
+            // then removing it will get a smaller number
+            // but we can only do so when k is larger than 0
+            for (int i = 0; i < num.Length; ++i)
             {
-                dp[i, 0] = "";
-            }
-            for (int i = 1; i <= num.Length; i++)
-            {
-                dp[0, i] = num.Substring(0, i);
-            }
-            for (int i = 1; i <= k; i++)
-            {
-                for (int j = 1; j <= num.Length; j++)
+                char c = num[i];
+                while (top > 0 && stk[top - 1] > c && k > 0)
                 {
-                    var curt = "";
-                    var s1 = dp[i - 1, j - 1].TrimStart(new[] { '0' });
-                    var s2 = (dp[i - 1, j - 1] + num[j - 1]).TrimStart(new[] { '0' });
-                    if (s1.Length > s2.Length || (s1.Length == s2.Length && s1.CompareTo(s2) > 0))
-                    {
-                        dp[i, j] = s2;
-                    }
-                    else
-                    {
-                        dp[i, j] = s1;
-                    }
+                    top -= 1;
+                    k -= 1;
                 }
+                stk[top++] = c;
             }
-            return dp[k, num.Length];
+            // find the index of first non-zero digit
+            int idx = 0;
+            while (idx < digits && stk[idx] == '0') idx++;
+            return idx == digits ? "0" : new string(stk, idx, digits - idx);
         }
 
         public static string RemoveKdigitsMy(string num, int k)
