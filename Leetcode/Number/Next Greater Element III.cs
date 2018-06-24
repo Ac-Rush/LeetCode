1363a9052578a2fd0de5bb8c896ca6f928c70e99
Kang Zhou <kazhou@microsoft.com>
2018-06-23 21:23:40 -0700

    Next_Greater_Element_III
    Next_Greater_Element_III

---
+++ Leetcode/Number/Next Greater Element III.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Number
+{
+    class Next_Greater_Element_III
+    {
+        /// <summary>
+        /// 太牛了，
+        ///  1. 从最右边找到第一个 降序的   i
+        /// 2. 从上个index，向右找到最后一个比这个大的   j
+        /// 3. 交换 i,j
+        /// 4翻转 i+1后边的书， 升序最小
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int NextGreaterElement(int n)
+        {
+            char[] a = ("" + n).ToCharArray();
+            int i = a.Length - 2;
+            while (i >= 0 && a[i + 1] <= a[i]) //1. 从最右边找到第一个 降序的  i
+            {
+                i--;
+            }
+            if (i < 0)
+                return -1;
+            int j = a.Length - 1;
+            while (j >= 0 && a[j] <= a[i]) //2. 从上个index，向右找到最后一个比这个大的  j
+            {
+                j--;
+            }
+            swap(a, i, j); //3.交换 i,j
+            reverse(a, i + 1);  //4翻转 i+1后边的书， 升序最小
+            try
+            {
+                return int.Parse(new String(a));
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+        private void reverse(char[] a, int start)
+        {
+            int i = start, j = a.Length - 1;
+            while (i < j)
+            {
+                swap(a, i, j);
+                i++;
+                j--;
+            }
+        }
+        private void swap(char[] a, int i, int j)
+        {
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
---
+++ Leetcode/Stack/Next Greater Element II.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Stack
+{
+    class Next_Greater_Element_II
+    {
+        /// <summary>
+        /// stack
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] nextGreaterElements(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            Stack<int> stack = new Stack<int>();
+            for (int i = 2 * nums.Length - 1; i >= 0; --i)
+            {
+                while (stack.Count > 0 && nums[stack.Peek()] <= nums[i % nums.Length])
+                {
+                    stack.Pop();
+                }
+                res[i % nums.Length] = stack.Count ==0 ? -1 : nums[stack.Peek()];
+                stack.Push(i % nums.Length);
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 暴力解法  N^2
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] NextGreaterElements(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = -1;
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[(i + j) % nums.Length] > nums[i])
+                    {
+                        res[i] = nums[(i + j) % nums.Length];
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
