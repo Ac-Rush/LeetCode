2bb55f8045826a689ddb9bac49e59908a7b1b4fc
Kang Zhou <kazhou@microsoft.com>
2018-06-24 18:56:38 -0700

    Max_Increase_to_Keep_City_Skyline
    Max_Increase_to_Keep_City_Skyline

---
+++ Leetcode/Matrix/Max Increase to Keep City Skyline.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Matrix
+{
+    class Max_Increase_to_Keep_City_Skyline
+    {
+        /// <summary>
+        ///  my solution
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            var left = new int[grid.Length];
+            var top = new int[grid[0].Length];
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    left[i] = Math.Max(left[i], grid[i][j]);
+                    top[j] = Math.Max(top[j], grid[i][j]);
+                }
+            }
+            var count = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    count += Math.Min(left[i], top[j]) - grid[i][j]; // my bug
+                }
+            }
+            return count;
+        }
+    }
+}
