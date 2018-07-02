f210061d93ad03a19b5282e48a3b2d57cb4d6f98
Kang Zhou <kazhou@microsoft.com>
2018-07-02 10:07:36 -0700

    MinSubArrayLen
    MinSubArrayLen

--- Leetcode/TwoPointer/Array Partition I.cs
+++ Leetcode/TwoPointer/Array Partition I.cs
@@ -8,5 +8,20 @@ namespace Leetcode.TwoPointer
 {
     class Array_Partition_I
     {
+        public int ArrayPairSum(int[] nums)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+            System.Array.Sort(nums);
+            var sum = 0;
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                sum += nums[i];
+            }
+            return sum;
+
+        }
     }
 }
