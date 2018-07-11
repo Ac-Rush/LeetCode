d8565ec67c990766ab59a68c31944f150c719fe6
Kang Zhou <kazhou@microsoft.com>
2018-07-11 15:37:42 -0700

    class Clone_Graph
        class Clone_Graph

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            var file = File.ReadAllLines(@"E:\tool\dri\machines.csv");
+            var list = File.ReadAllText(@"E:\tool\dri\list.txt");
+            var set = new HashSet<string>(list.Split(new []{','}));
+
+            using (System.IO.StreamWriter output =
+            new System.IO.StreamWriter(@"E:\tool\dri\machines2.csv", true))
+            {
+                foreach (var line in file)
+                {
+                    if (!line.StartsWith("#"))
+                    {
+                        var seg = line.Split(new[] {','});
+                        if (!set.Contains(seg[0]))
+                        {
+                            output.WriteLine(line);
+                        }
+                        else
+                        {
+                            seg[2] = "NU";
+                            seg[3] = "0";
+                            output.WriteLine(string.Join(",",seg));
+                        }
+                    }
+                    else
+                    {
+
+                        output.WriteLine(line);
+                    }
+
+                }
+            }
         }
     }
 }
---
+++ Leetcode/Graph/Clone Graph.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Graph
+{
+    class Clone_Graph
+    {
+        public class UndirectedGraphNode
+        {
+            public int label;
+            public IList<UndirectedGraphNode> neighbors;
+
+            public UndirectedGraphNode(int x)
+            {
+                label = x;
+                neighbors = new List<UndirectedGraphNode>();
+            }
+        }
+        /// <summary>
+        /// 哈哈哈，我的答案一次过
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public UndirectedGraphNode CloneGraph(UndirectedGraphNode node)
+        {
+            if (node == null) return node; //my bug 没有 check null， 不过我是知道的
+            var dict = new Dictionary<UndirectedGraphNode, UndirectedGraphNode>(); //dict 保存mapping
+            var visited = new HashSet<UndirectedGraphNode>(); //visited去重
+            var queue = new Queue<UndirectedGraphNode>() ; //queue 用来BFS
+            queue.Enqueue(node);
+            //先复制点
+            while (queue.Any())
+            {
+                var u = queue.Dequeue();
+                visited.Add(u);
+                var newU = new UndirectedGraphNode(u.label);
+                dict[u] = newU;
+                foreach (var neighbor in u.neighbors)
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+            //再复制边
+            foreach (var u in dict.Keys)
+            {
+                foreach (var neighbor in u.neighbors)
+                {
+                    dict[u].neighbors.Add(dict[neighbor]);
+                }
+            }
+            return dict[node];
+
+        }
+    }
+}
