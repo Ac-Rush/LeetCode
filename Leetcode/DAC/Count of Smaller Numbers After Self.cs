bff517c46e8e6502e7aca642c08dd1a69bfc3c80
Kang Zhou <kazhou@microsoft.com>
2018-07-10 22:15:34 -0700

    Count_of_Smaller_Numbers_After_Self_BST
    Count_of_Smaller_Numbers_After_Self_BST

--- Leetcode/DAC/Count of Smaller Numbers After Self.cs
+++ Leetcode/DAC/Count of Smaller Numbers After Self.cs
@@ -49,7 +49,113 @@ namespace Leetcode.DAC
         }
     }
 
+    /// <summary>
+    /// What is Binary Index Tree
+    /// </summary>
+    /*
+     1, we should build an array with the length equals to the max element of the nums array as BIT.
+2, To avoid minus value in the array, we should first add the (min+1) for every elements 
+(It may be out of range, where we can use long to build another array. But no such case in the test cases so far.)
+3, Using standard BIT operation to solve it.
+
+    */
     class Count_of_Smaller_Numbers_After_Self_NLogN
     {
+        public List<int> CountSmaller(int[] nums)
+        {
+            List<int> res = new List<int>();
+            if (nums == null || nums.Length == 0)
+            {
+                return res;
+            }
+            // find min value and minus min by each elements, plus 1 to avoid 0 element
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                min = (nums[i] < min) ? nums[i] : min;
+            }
+            int[] nums2 = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums2[i] = nums[i] - min + 1;
+                max = Math.Max(nums2[i], max);
+            }
+            int[] tree = new int[max + 1];
+            for (int i = nums2.Length - 1; i >= 0; i--)
+            {
+                res.Insert(0, get(nums2[i] - 1, tree));
+                update(nums2[i], tree);
+            }
+            return res;
+        }
+
+        private int get(int i, int[] tree)
+        {
+            int num = 0;
+            while (i > 0)
+            {
+                num += tree[i];
+                i -= i & (-i);
+            }
+            return num;
+        }
+        private void update(int i, int[] tree)
+        {
+            while (i < tree.Length)
+            {
+                tree[i]++;
+                i += i & (-i);
+            }
+        }
+    }
+
+
+    class Count_of_Smaller_Numbers_After_Self_BST
+    {
+        public class Node
+        {
+            public Node left, right;
+            public int val, sum, dup = 1;
+            public Node(int v, int s)
+            {
+                val = v;
+                sum = s;
+            }
+        }
+        public List<int> CountSmaller(int[] nums)
+        {
+            var ans = new int[nums.Length];
+            Node root = null;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                root = insert(nums[i], root, ans, i, 0);
+            }
+            return ans.ToList();
+        }
+
+        private Node insert(int num, Node node, int[] ans, int i, int preSum)
+        {
+            if (node == null)
+            {
+                node = new Node(num, 0);
+                ans[i] = preSum;
+            }
+            else if (node.val == num)
+            {
+                node.dup++;
+                ans[i] = preSum + node.sum;
+            }
+            else if (node.val > num)
+            {
+                node.sum++;
+                node.left = insert(num, node.left, ans, i, preSum);
+            }
+            else
+            {
+                node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+            }
+            return node;
+        }
     }
 }
