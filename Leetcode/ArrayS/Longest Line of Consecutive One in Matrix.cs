3e72a4753a52f38d893e2d44cbdff5538450a461
Kang Zhou <kazhou@microsoft.com>
2018-07-19 15:17:13 -0700

    其实还是个暴力解法
    其实还是个暴力解法

---
+++ Leetcode/ArrayS/Longest Line of Consecutive One in Matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayS
+{
+    class Longest_Line_of_Consecutive_One_in_Matrix
+    {
+        //用最后一维度来保存 4中 state
+        public int LongestLine(int[,] M)
+        {
+            var m = M.GetLength(0);
+            var n = M.GetLength(1);
+            var max = 0;
+            var dp = new int[m,n,4]; //这样表示就好了
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (M[i,j] == 0) continue;
+                    for (int k = 0; k < 4; k++) dp[i,j,k] = 1;
+                    if (j > 0) dp[i,j,0] += dp[i,j - 1,0]; // horizontal line
+                    if (j > 0 && i > 0) dp[i,j,1] += dp[i - 1,j - 1,1]; // anti-diagonal line
+                    if (i > 0) dp[i,j,2] += dp[i - 1,j,2]; // vertical line
+                    if (j < m - 1 && i > 0) dp[i,j,3] += dp[i - 1,j + 1,3]; // diagonal line
+                    max = Math.Max(max, Math.Max(dp[i,j,0], dp[i,j,1]));
+                    max = Math.Max(max, Math.Max(dp[i,j,2], dp[i,j,3]));
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Leetcode/Dynamic Programming/Guess Number Higher or Lower II.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    /// <summary>
+    /// Approach #3 Using DP [Accepted]
+    /// N^3的时间复杂度
+    /// </summary>
+    class Guess_Number_Higher_or_Lower_II_Dp_Better
+    {
+        public int GetMoneyAmount(int n)
+        {
+            var dp = new int[n + 1, n + 1];
+            for (int len = 2; len <= n; len++) // 对于这种 range 来做的DP， 要根据 len 来 loop
+            {
+                for (int start = 1; start <= n - len + 1; start++)
+                {
+                    int minres = Int32.MaxValue;
+                    for (int piv = start + (len - 1) / 2; piv < start + len - 1; piv++) //修改 开始点来判断
+                    {
+                        int res = piv + Math.Max(dp[start, piv - 1], dp[piv + 1, start + len - 1]);
+                        minres = Math.Min(res, minres);
+                    }
+                    dp[start, start + len - 1] = minres;
+                }
+            }
+            return dp[1, n];
+        }
+
+    }
+
+
+    /// <summary>
+    /// Approach #3 Using DP [Accepted]
+    /// N^3的时间复杂度
+    /// </summary>
+    class Guess_Number_Higher_or_Lower_II_Dp
+    {
+        public int GetMoneyAmount(int n)
+        {
+            var dp = new int[n + 1,n + 1];
+            for (int len = 2; len <= n; len++) // 对于这种 range 来做的DP， 要根据 len 来 loop
+            {
+                for (int start = 1; start <= n - len + 1; start++)
+                {
+                    int minres = Int32.MaxValue;
+                    for (int piv = start; piv < start + len - 1; piv++)
+                    {
+                        int res = piv + Math.Max(dp[start,piv - 1], dp[piv + 1,start + len - 1]);
+                        minres = Math.Min(res, minres);
+                    }
+                    dp[start,start + len - 1] = minres;
+                }
+            }
+            return dp[1,n];
+        }
+       
+    }
+
+    /// <summary>
+    /// Approach #3 Using DP [Accepted]
+    /// N^3的时间复杂度
+    /// </summary>
+    class Guess_Number_Higher_or_Lower_II_Memo
+    {
+        private int?[,] memo;
+        public int GetMoneyAmount(int n)
+        {
+            memo = new int?[n + 1,n + 1];
+            return calculate(1, n);
+        }
+        public int calculate(int low, int high)
+        {
+            if (low >= high)
+                return 0;
+            if (memo[low, high] != null)
+            {
+                return memo[low, high].Value;
+            }
+            int minres = Int32.MaxValue;
+            for (int i = low; i <= high; i++)
+            {
+                int res = i + Math.Max(calculate(i + 1, high), calculate(low, i - 1));  //构造递归解空间，遍历每个i， i+ (i+1, high) 还是 (low, i-1)
+                minres = Math.Min(res, minres);
+            }
+            memo[low, high] = minres;
+            return minres;
+        }
+    }
+
+    /// <summary>
+    /// 首先想的应该是暴力解法
+    /// Approach #1 Brute Force [Time Limit Exceeded]
+    /// N！时间复杂度
+    /// </summary>
+    class Guess_Number_Higher_or_Lower_II_BF
+    {
+        public int GetMoneyAmount(int n)
+        {
+            return calculate(1, n);
+        }
+        public int calculate(int low, int high)
+        {
+            if (low >= high)
+                return 0;
+            int minres = Int32.MaxValue;
+            for (int i = low; i <= high; i++)
+            {
+                int res = i + Math.Max(calculate(i + 1, high), calculate(low, i - 1));  //构造递归解空间，遍历每个i， i+ (i+1, high) 还是 (low, i-1)
+                minres = Math.Min(res, minres);
+            }
+
+            return minres;
+        }
+    }
+}
