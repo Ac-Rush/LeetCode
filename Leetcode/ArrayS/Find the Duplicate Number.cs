ea684da0bbf656a10ce536a1462802e4986b62d3
justboy1987 <175636422@qq.com>
2018-11-19 08:11:28 +0800

    Find_the_Duplicate_Number
    Find_the_Duplicate_Number

---
+++ Leetcode/ArrayS/Find the Duplicate Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayS
+{
+    class Find_the_Duplicate_Number
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            // Find the intersection point of the two runners.
+            int tortoise = nums[0];
+            int hare = nums[0];
+            do
+            {
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+            } while (tortoise != hare);
+
+            // Find the "entrance" to the cycle.
+            int ptr1 = nums[0];
+            int ptr2 = tortoise;
+            while (ptr1 != ptr2)
+            {
+                ptr1 = nums[ptr1];
+                ptr2 = nums[ptr2];
+            }
+
+            return ptr1;
+        }
+    }
+}
