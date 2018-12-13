f42d016611b2e239120f2c98559db6437ffbd8dd
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2018-12-12 20:34:47 -0800

    TwoSumSolution
    TwoSumSolution

--- Leetcode/ArrayS/TwoSum.cs
+++ Leetcode/ArrayS/TwoSum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Leetcode.Array
 {
@@ -17,6 +18,27 @@ namespace Leetcode.Array
                 }
             }
             throw new ArgumentException("No two sum solution");
+        }
+    }
+
+    public class TwoSumSolution2
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(target - nums[i]))
+                {
+                    result[1] = i;
+                    result[0] = map[target - nums[i]];
+                    return result;
+                }
+                map[nums[i]]= i;   //这里一定要边查边放
+            }
+            return result;
+            
         }
     }
 }
