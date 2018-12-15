da7f8e2ee720558fc8f7e15d81ec0e3ebb68a153
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2018-12-15 13:23:11 -0800

    _4Sum_II
    _4Sum_II

--- Leetcode/ArrayS/Heaters.cs
+++ Leetcode/ArrayS/Heaters.cs
@@ -9,6 +9,15 @@ namespace Leetcode.Array
 {
     class Heaters
     {
+        /*
+         *
+         * The idea is to leverage decent Arrays.binarySearch() function provided by Java.
+
+For each house, find its position between those heaters (thus we need the heaters array to be sorted).
+Calculate the distances between this house and left heater and right heater, get a MIN value of those two values. Corner cases are there is no left or right heater.
+Get MAX value among distances in step 2. It's the answer.
+Time complexity: max(O(nlogn), O(mlogn)) - m is the length of houses, n is the length of heaters.
+         */
         public int FindRadius(int[] houses, int[] heaters)
         {
             System.Array.Sort(heaters);
@@ -21,9 +30,11 @@ namespace Leetcode.Array
                 {
                     index = -(index + 1);
                 }
+                // 左边暖气的距离
                 int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : int.MaxValue;
+                //右边暖气的距离 
                 int dist2 = index < heaters.Length ? heaters[index] - house : int.MaxValue;
-
+                
                 result = Math.Max(result, Math.Min(dist1, dist2));
             }
 
