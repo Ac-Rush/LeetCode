fa2e2c0ec24a1d377499fcae825b3b7546d27de4
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2018-12-15 18:38:44 -0800

    N_ary_Tree_Postorder_Traversal
    N_ary_Tree_Postorder_Traversal

---
+++ Leetcode/Tree/Merge Two Binary Trees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Merge_Two_Binary_Trees
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+            {
+                return null;
+            }
+            else if (t2 == null)
+            {
+                return t1;
+            }
+            else if (t1 == null)
+            {
+                return t2;
+            }
+            else
+            {
+                var t = new TreeNode(t1.val + t2.val)
+                {
+                    left = MergeTrees(t1.left, t2.left),
+                    right = MergeTrees(t1.right, t2.right)
+                };
+                return t;
+            }
+        }
+    }
+}
