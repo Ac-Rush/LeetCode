bf35b77410491c66e41a4f548cdaa015d1780e4d
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2018-12-15 22:57:49 -0800

    Sort_Array_By_Parity_II
    Sort_Array_By_Parity_II

---
+++ Leetcode/ArrayS/Sort Array By Parity II.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayS
+{
+    class Sort_Array_By_Parity_II
+    {
+        public int[] SortArrayByParityII(int[] A)
+        {
+            int e = 0;
+            int o = 1;
+
+            while (e < A.Length && o < A.Length)
+            {
+                if (A[e] % 2 != 0)
+                {
+                    var t = A[o];
+                    A[o] = A[e];
+                    A[e] = t;
+                    o += 2;
+                }
+                else
+                {
+                    e += 2;
+                }
+            }
+
+            return A;
+        }
+    }
+}
