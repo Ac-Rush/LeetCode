22dedb3e20c079ffd1f619fe6454a181061ec2ea
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2018-12-15 21:57:09 -0800

    Contains_Duplicate_III_Bucket
    Contains_Duplicate_III_Bucket

--- Leetcode/buckets/Contains Duplicate III.cs
+++ Leetcode/buckets/Contains Duplicate III.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace Leetcode.BFS
+namespace Leetcode.buckets
 {
     class Contains_Duplicate_III_Bucket
     {
@@ -23,16 +19,20 @@ When looking up a value, check which bucket it would go to, and its neighbors. I
         /// <returns></returns>
         public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
         {
+
             if (k < 1 || t < 0) return false;
             var map = new Dictionary<long, long>();  //《桶，值》
             for (int i = 0; i < nums.Length; i++)
             {
                 long remappedNum = (long)nums[i] - int.MinValue;  //或者减去 min也可以
+
+                //按 t+1分桶
                 long bucket = remappedNum / ((long)t + 1);
                 if (map.ContainsKey(bucket)
                         || (map.ContainsKey(bucket - 1) && remappedNum - map[bucket - 1] <= t)
                             || (map.ContainsKey(bucket + 1) && map[bucket + 1] - remappedNum <= t))
                     return true;
+                //去除 K之前的数据
                 if (map.Count >= k)
                 {
                     long lastBucket = ((long)nums[i - k] - int.MinValue) / ((long)t + 1);
