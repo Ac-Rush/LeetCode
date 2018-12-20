1d171787df9e02c8addece83a27c3660084408b3
justboy1987 <175636422@qq.com>
2018-12-20 14:03:17 +0800

    Roman_to_Integer
    Roman_to_Integer

---
+++ Leetcode/LinkList/Delete Node in a Linked List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Delete_Node_in_a_Linked_List
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
