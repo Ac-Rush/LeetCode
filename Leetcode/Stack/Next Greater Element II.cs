8af53ab9e85536a95b51a16ed6d3e1c496ed923a
Kang Zhou <kazhou@microsoft.com>
2018-12-21 14:55:07 -0800

    NextGreaterElement2
    NextGreaterElement2

--- Leetcode/Stack/Next Greater Element II.cs
+++ Leetcode/Stack/Next Greater Element II.cs
@@ -10,7 +10,7 @@ namespace Leetcode.Stack
     class Next_Greater_Element_II
     {
         /// <summary>
-        /// stack  两杯数组的解法
+        /// stack  两倍数组的解法
         /// 这个也得看看
         /// </summary>
         /// <param name="nums"></param>
