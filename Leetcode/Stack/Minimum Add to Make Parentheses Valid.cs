ceab81d9dd31f78e70f223888aabe6c6473c689b
justboy1987 <175636422@qq.com>
2018-12-24 07:55:33 +0800

    Minimum_Add_to_Make_Parentheses_Valid
    Minimum_Add_to_Make_Parentheses_Valid

---
+++ Leetcode/Stack/Minimum Add to Make Parentheses Valid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Stack
+{
+    class Minimum_Add_to_Make_Parentheses_Valid
+    {
+        public int MinAddToMakeValid(string S)
+        {
+            //count 需要的左括号
+            //stk 需要的右括号
+            int count = 0, stk = 0;
+            foreach(var c in S)
+            {
+                if (c == '(') { ++stk; }
+                else if (stk <= 0) { ++count; }
+                else { --stk; }
+            }
+            return count + stk;
+        }
+    }
+}
