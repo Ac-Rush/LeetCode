0aff2c82bb2830a2681cceaa38007fc731ea1196
justboy1987 <175636422@qq.com>
2018-12-24 13:12:27 +0800

    StoneGame
    StoneGame

---
+++ Leetcode/Dynamic Programming/Stone Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    class Stone_Game
+    {
+        public bool StoneGame(int[] p)
+        {
+            int n = p.Length;
+            int[,] dp = new int[n,n];
+            for (int i = 0; i < n; i++) dp[i,i] = p[i];
+            for (int d = 1; d < n; d++)
+                for (int i = 0; i < n - d; i++)
+                    dp[i,i + d] = Math.Max(p[i] - dp[i + 1,i + d], p[i + d] - dp[i,i + d - 1]);
+            return dp[0,n - 1] > 0;
+        }
+    }
+}
---
+++ Leetcode/Number/Ugly Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Number
+{
+    class Ugly_Number
+    {
+        public bool IsUgly(int num)
+        {
+            while (num > 1 && num % 5 == 0) num /= 5;
+            while (num > 1 && num % 3 == 0) num /= 3;
+            while (num > 1 && num % 2 == 0) num /= 2;
+            return num == 1;
+        }
+
+        public bool IsUgly2(int num)
+        {
+            for (int i = 2; i < 6 && num > 0; i++)
+                while (num % i == 0)
+                    num /= i;
+            return num == 1;
+        }
+    }
+}
