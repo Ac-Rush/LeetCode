7bd10dcea66a7d1f439eb3d200f302e61c556e68
justboy1987 <175636422@qq.com>
2018-12-26 12:07:46 +0800

    Remove_Linked_List_Elements
    Remove_Linked_List_Elements

---
+++ Leetcode/LinkList/Remove Linked List Elements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Remove_Linked_List_Elements
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var demy = new ListNode(0);
+            demy.next = head;
+            var cur = demy;
+            while (cur.next != null)
+            {
+                if (cur.next.val == val)
+                {
+                    cur.next = cur.next.next;
+                }
+                else
+                {
+                    cur = cur.next; //这个应该在 else里面
+                }
+            }
+            return demy.next;
+        }
+    }
+}
