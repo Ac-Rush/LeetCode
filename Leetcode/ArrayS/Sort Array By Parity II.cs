33595bc9d7c3a13763df4fdc78962525b517a2c0
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-03-03 21:56:40 -0800

    Sort_Array_By_Parity_II_v2
    Sort_Array_By_Parity_II_v2

--- Leetcode/ArrayS/Sort Array By Parity II.cs
+++ Leetcode/ArrayS/Sort Array By Parity II.cs
@@ -8,6 +8,11 @@ namespace Leetcode.ArrayS
 {
     class Sort_Array_By_Parity_II
     {
+        /// <summary>
+        /// 这个不是太直接， 
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
         public int[] SortArrayByParityII(int[] A)
         {
             int e = 0;
@@ -17,6 +22,7 @@ namespace Leetcode.ArrayS
             {
                 if (A[e] % 2 != 0)
                 {
+                    //换完， o也不见得是偶数，所以这边的e不加2， 进到下次的 while， 总之能使 e最终是偶数
                     var t = A[o];
                     A[o] = A[e];
                     A[e] = t;
@@ -29,6 +35,33 @@ namespace Leetcode.ArrayS
             }
 
             return A;
+        }
+    }
+    /// <summary>
+    /// 双指针更好理解版本
+    /// 就像partition， 找到两个不符合要求的交换
+    /// </summary>
+    class Sort_Array_By_Parity_II_v2
+    {
+        public int[] SortArrayByParityII(int[] A)
+        {
+            int i = 0, j = 1, n = A.Length;
+            while (i < n && j < n)
+            {
+                while (i < n && A[i] % 2 == 0) i += 2;
+                while (j < n && A[j] % 2 == 1) j += 2;
+                if (i < n && j < n) //my bug还需要 这里的判断
+                {
+                    Swap(A, i, j);
+                }
+            }
+            return A;
+        }
+        private void Swap(int[] A, int i, int j)
+        {
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
         }
     }
 }
---
+++ Leetcode/Sort/Squares of a Sorted Array.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Sort
+{
+    class Squares_of_a_Sorted_Array
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            return A.Select(i => i * i).OrderBy(i => i).ToArray();
+        }
+    }
+
+    /// <summary>
+    /// two pointer
+    /// 因为原始数组已经有序， 左边或是右边的平方 肯定是最大的，所以用了two pointer
+    /// 
+    /// </summary>
+
+    class Squares_of_a_Sorted_Array_two_pointer
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            int n = A.Length;
+            int[] result = new int[n];
+            int i = 0, j = n - 1;
+            for (int p = n - 1; p >= 0; p--)
+            {
+                if (Math.Abs(A[i]) > Math.Abs(A[j]))
+                {
+                    result[p] = A[i] * A[i];
+                    i++;
+                }
+                else
+                {
+                    result[p] = A[j] * A[j];
+                    j--;
+                }
+            }
+            return result;
+        }
+    }
+
+    class Squares_of_a_Sorted_Array_two_pointer_simple
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            int i = 0;
+            var result = new int[A.Length];
+            for (int p = A.Length -1 , j = p , i = 0; p >= 0; p--)  result[p] = A[i] * A[i] > A[j] * A[j] ? A[i] * A[i++] : A[j] * A[j--];
+            return result;
+        }
+    }
+}
