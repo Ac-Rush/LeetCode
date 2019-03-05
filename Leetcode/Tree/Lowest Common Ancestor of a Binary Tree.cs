292f2072fc6ccff8549b42d596e5322061a56535
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-03-05 08:59:03 -0800

    Lowest_Common_Ancestor_of_a_Binary_Tree
    Lowest_Common_Ancestor_of_a_Binary_Tree

--- Leetcode/Tree/Lowest Common Ancestor of a Binary Tree.cs
+++ Leetcode/Tree/Lowest Common Ancestor of a Binary Tree.cs
@@ -17,9 +17,16 @@ namespace Leetcode.Tree
         /// <returns></returns>
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
+            //  空或者有一个是 就返回root
             if (root == null || root == p || root == q) return root;
+            //看左边
             TreeNode left = LowestCommonAncestor(root.left, p, q);
+            //看右边
             TreeNode right = LowestCommonAncestor(root.right, p, q);
+            // 1 不在左边 ， 返回右边
+            // 2， 在左边，
+            //       2.a 右边 不在，返回左边
+            //       2.b 右边在， 返回root
             return left == null ? right : right == null ? left : root;
         }
     }
