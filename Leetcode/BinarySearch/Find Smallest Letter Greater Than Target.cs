248e526f137ffdb60483d522a312e9f979fb3e6f
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-03-17 16:35:56 -0700

    int FindDuplicate3(int[] nums)
            int FindDuplicate3(int[] nums)

--- Leetcode/BinarySearch/Find Smallest Letter Greater Than Target.cs
+++ Leetcode/BinarySearch/Find Smallest Letter Greater Than Target.cs
@@ -9,16 +9,35 @@ namespace Leetcode.BinarySearch
 {
     class Find_Smallest_Letter_Greater_Than_Target
     {
+        //经典
+        /// <summary>
+        /// 左闭右开的经典二分搜索模板
+        /// </summary>
+        /// <param name="letters"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public char NextGreatestLetter(char[] letters, char target)
         {
-            int lo = 0, hi = letters.Length;
-            while (lo < hi)
+            //hi starts at 'n' rather than the usual 'n - 1'. 
+            //It is because the terminal condition is 'lo < hi' and if hi starts from 'n - 1', 
+            //we can never consider value at index 'n - 1'
+            int low = 0, high = letters.Length;  //这个超赞high = A.Length 而不是 high = A.Length -1, 这是因为 low 不能等high， 所以不会越界， 这样可以找到第一个大的，如果不存在就返回 length.
+            while (low < high)
             {
-                int mi = lo + (hi - lo) / 2;
-                if (letters[mi] <= target) lo = mi + 1;
-                else hi = mi;
+                int mid = low + ((high - low) >> 1);
+                if (letters[mid] > target)
+                {
+                    //should not be mid-1 when A[mid]==target.
+                    //could be mid even if A[mid]>target because mid<high.
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
             }
-            return letters[lo % letters.Length];
+            //Because lo can end up pointing to index 'n', in which case we return the first element
+            return letters[low % letters.Length];
         }
     }
 }
