f832da3cb000aa36475f041b40171c77de190599
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-03-17 12:08:58 -0700

    Leetcode
    Leetcode

--- Leetcode/BinarySearch/Find K Closest Elements.cs
+++ Leetcode/BinarySearch/Find K Closest Elements.cs
@@ -17,17 +17,6 @@ namespace Leetcode.BinarySearch
         /// <returns></returns>
         public static IList<int> FindClosestElements(int[] arr, int k, int x)
         {
-            if (arr[0] >= x)
-            {
-                return arr.Take(k).ToList();
-            }
-            if (arr[arr.Length - 1] <= x)
-            {
-                return arr.Skip(arr.Length - k).Take(k).ToList();
-            }
-
-            var start = 0;
-            var end = arr.Length - 1;
             var index = System.Array.BinarySearch(arr, x);
             if (index < 0)
             {
@@ -41,31 +30,75 @@ namespace Leetcode.BinarySearch
                 if (left < 0)
                 {
                     right++;
-                    count++;
                     
                 }else if (right >= arr.Length)
                 {
                     left --;
-                    count++;
                 }
                 else
                 {
                     if (x - arr[left] <= arr[right] - x)
                     {
                         left--;
-                        count++;
                     }
                     else
                     {
                         right++;
-                        count++;
                     }
                 }
-                
+                count++;
             }
             return arr.Skip(left + 1).Take(k).ToList();
         }
 
-        
+
+
+        public static IList<int> FindClosestElements2(int[] arr, int k, int x)
+        {
+            var ans = new List<int>();
+            var l = 0;
+            var r = arr.Length - 1;
+            while (l < r)
+            {
+                var m = (l + r) / 2;
+                if (arr[m] >= x)
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+            var count = 0;
+            var left = l - 1;
+            var right = l;
+            while (count < k)
+            {
+                if (left < 0)
+                {
+                    right++;
+                }
+                else if (right >= arr.Length)
+                {
+                    left--;
+                }
+                else
+                {
+                    if (x - arr[left] <= arr[right] - x)
+                    {
+                        left--;
+                    }
+                    else
+                    {
+                        right++;
+                    }
+                }
+                count++;
+
+            }
+            return arr.Skip(left + 1).Take(k).ToList();
+        }
+
     }
 }
--- Leetcode/LinkList/Merge Two Sorted Lists.cs
+++ Leetcode/LinkList/Merge Two Sorted Lists.cs
@@ -31,4 +31,25 @@ namespace Leetcode.LinkList
             return demy.next;
         }
     }
+
+    class Merge_Two_Sorted_Lists_Recursive
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val <= l2.val)
+            {
+                var next = l1.next;
+                l1.next = MergeTwoLists(next, l2);
+                return l1;
+            }
+            else
+            {
+                var next = l2.next;
+                l2.next = MergeTwoLists(l1, next);
+                return l2;
+            }
+        }
+    }
 }
