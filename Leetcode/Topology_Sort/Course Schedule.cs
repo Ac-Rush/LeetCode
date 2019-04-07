79c52c60c071cfa6ed15bfe9921e1fab03c1e3ee
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-04-06 22:17:18 -0700

    FindOrder
    FindOrder

--- Leetcode/Topology_Sort/Course Schedule.cs
+++ Leetcode/Topology_Sort/Course Schedule.cs
@@ -59,4 +59,60 @@ namespace Leetcode.Graph
             return count == numCourses;
         }
     }
+
+    class Course_Schedule_my
+    {
+        //明天好好看看
+        /// <summary>
+        /// Easy BFS Topological sort, Java
+        /// BFS 拓扑排序， sort
+        /// </summary>
+        /// <param name="numCourses"></param>
+        /// <param name="prerequisites"></param>
+        /// <returns></returns>
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            var matrix = new int[numCourses, numCourses]; // i -> j
+            int[] indegree = new int[numCourses];
+
+            
+            //1. 构建有向图， 并且统计入度
+            foreach (var prerequisite in prerequisites)
+            {
+                int ready = prerequisite[0];
+                int pre = prerequisite[1];
+                if (matrix[pre, ready] == 0)
+                    indegree[ready]++; //duplicate case  增加入度
+                matrix[pre, ready] = 1;
+            }
+
+            //2. 拓扑排序
+            int count = 0;
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < indegree.Length; i++)
+            {
+                //把 0入度的添加到 Queue里
+                if (indegree[i] == 0) queue.Enqueue(i);
+            }
+            while (queue.Any())
+            {
+                //取出 0入度的课程
+                int course = queue.Dequeue();
+                count++; //这个课没有 前序， 所以课程+1
+                for (int i = 0; i < numCourses; i++)
+                {
+                    //遍历所有课， 如果有 课程i需要 course为前缀
+                    if (matrix[course, i] != 0)
+                    {
+                        //那么把课程i的前缀 减一
+                        if (--indegree[i] == 0)
+                            //把 入度为0的加入queue.
+                            queue.Enqueue(i);
+                    }
+                }
+            }
+            //3. 判断，返回是不是遍历了需要的课程
+            return count == numCourses;
+        }
+    }
 }
