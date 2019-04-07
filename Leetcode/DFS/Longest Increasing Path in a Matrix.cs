6b441db56a385ecfabbd57e8550dc59828c358f5
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-04-06 22:45:37 -0700

    Longest_Increasing_Path_in_a_Matrix2
    Longest_Increasing_Path_in_a_Matrix2

--- Leetcode/DFS/Longest Increasing Path in a Matrix.cs
+++ Leetcode/DFS/Longest Increasing Path in a Matrix.cs
@@ -15,23 +15,23 @@ namespace Leetcode.DFS_BFS
         /// <summary>
         /// memorization  记忆化搜索，还是用DFS去暴力搜索， 
         /// </summary>
-        public static int[,] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-       
-        public static int LongestIncreasingPath(int[,] matrix)
+        public  int[,] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        public int[][] dirs2 = { new []{ 0, 1 }, new[] { 1, 0 }, new[] { 0, -1 }, new[] { -1, 0 } };
+        public  int LongestIncreasingPath(int[][] matrix)
         {
+            if (matrix == null || matrix.Length == 0) return 0;
             var max = 0;
-            var meno = new int[matrix.GetLength(0), matrix.GetLength(1)];
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            var meno = new int[matrix.Length, matrix[0].Length];
+            for (int i = 0; i < matrix.Length; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix[0].Length; j++)
                 {
                     meno[i, j] = -1;
                 }
             }
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < matrix.Length; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix[i].Length; j++)
                 {
                     max = Math.Max(max, Dfs(meno, matrix, i, j));
                 }
@@ -39,7 +39,7 @@ namespace Leetcode.DFS_BFS
             return max;
         }
 
-        private static int Dfs(int[,] memo, int[,] matrix, int r, int c)
+        private  int Dfs(int[,] memo, int[][] matrix, int r, int c)
         {
             if (memo[r, c] != -1)
             {
@@ -50,9 +50,9 @@ namespace Leetcode.DFS_BFS
             {
                 var rr = r + dirs[i, 0];
                 var cc = c + dirs[i, 1];
-                if (0 <= rr && rr < matrix.GetLength(0)
-                    && 0 <= cc && cc < matrix.GetLength(1)
-                    && matrix[r, c] > matrix[rr, cc])
+                if (0 <= rr && rr < matrix.Length
+                    && 0 <= cc && cc < matrix[0].Length
+                    && matrix[r][ c] > matrix[rr][ cc])
                 {
                     len = Math.Max(len, Dfs(memo,matrix, rr, cc));
                 }
