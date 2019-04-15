ebc4c7012d6e8410abf52e2b97868021636cabe0
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-04-14 22:22:02 -0700

    LinkList
    LinkList

---
+++ Leetcode/LinkList/Add Two Numbers II.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Add_Two_Numbers_II
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            Stack<int> s1 = new Stack<int>();
+            Stack<int> s2 = new Stack<int>();
+
+            while (l1 != null)
+            {
+                s1.Push(l1.val);
+                l1 = l1.next;
+            };
+            while (l2 != null)
+            {
+                s2.Push(l2.val);
+                l2 = l2.next;
+            }
+
+            int sum = 0;
+            ListNode list = new ListNode(0);
+            while (s1.Any() || s2.Any())
+            {
+                if (s1.Any()) sum += s1.Pop();
+                if (s2.Any()) sum += s2.Pop();
+                list.val = sum % 10;
+                ListNode head = new ListNode(sum / 10);
+                head.next = list;
+                list = head;
+                sum /= 10;
+            }
+
+            return list.val == 0 ? list.next : list;
+        }
+    }
+}
