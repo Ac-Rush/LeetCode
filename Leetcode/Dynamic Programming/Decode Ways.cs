c342e8e5bebe2e8a421a486e660f0637e312f615
Yogish Murthy <ykrish@microsoft.com>
2019-04-30 14:59:56 -0700

    Decode_Ways_II
    Decode_Ways_II

--- Leetcode/Dynamic Programming/Decode Ways.cs
+++ Leetcode/Dynamic Programming/Decode Ways.cs
@@ -22,18 +22,40 @@ namespace Leetcode.Dynamic_Programming
                 // possible two-digit letter, so new r1 is sum of both while new r2 is the old r1
                 if (s[i - 1] == '1' || s[i - 1] == '2' && s[i] <= '6')
                 {
-                    r1 = r2 + r1;
-                    r2 = r1 - r2;
+                    r1 = r2 + r1; //r1 表示当前，r1 就等于之前的 r1 和 r2的和
+                    r2 = r1 - r2; // r2就等于 之前的r1
                 }
 
                 // one-digit letter, no new way added
                 else
                 {
-                    r2 = r1;
+                    r2 = r1;  //否则更新 r2
                 }
             }
 
             return r1;
-        } 
+        }
+
+
+        public int NumDecodings2(string s)
+        {
+            int n = s.Length, first = 1, second = 1;
+            if (n == 0 || s[0] == '0') return 0;
+            for (int i = 1; i < n; i++)
+            {
+
+                if (s[i] == '0') second = 0;
+                if (s[i] <= '6' && s[i - 1] == '2' || s[i - 1] == '1') // my bug 
+                {
+                    second = first + second;  //这样替换 不需要中间变量
+                    first = second - first;
+                }
+                else
+                {
+                    first = second;  //my bug
+                }
+            }
+            return second;
+        }
     }
 }
