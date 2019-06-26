9021389250eee2fe2cb97e689f85cc83c304ce33
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-06-25 17:32:46 -0700

    class _1018__Binary_Prefix_Divisible_By_5
        class _1018__Binary_Prefix_Divisible_By_5

---
+++ Leetcode/ArrayS/[1018] Binary Prefix Divisible By 5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayS
+{
+    class _1018__Binary_Prefix_Divisible_By_5
+    {
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            var ans = new List<bool>();
+            var rest = 0;
+            foreach (var n in A)
+            {
+                rest = rest * 2 + n;
+                ans.Add(rest % 5 == 0);
+                rest %= 5;
+            }
+            return ans;
+        }
+    }
+}
