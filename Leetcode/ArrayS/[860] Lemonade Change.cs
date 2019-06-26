3537801853d0263cafb20de484a1e649e97e7f44
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-06-26 16:34:50 -0700

    _860__Lemonade_Change
    _860__Lemonade_Change

---
+++ Leetcode/ArrayS/[860] Lemonade Change.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayS
+{
+    class _860__Lemonade_Change
+    {
+        public bool LemonadeChange(int[] bills)
+        {
+            int[] rest = new int[3]; //5 ,10 ,20
+            foreach (var bill in bills)
+            {
+                switch (bill)
+                {
+                    case 5:
+                        {
+                            rest[0]++;
+                            break;
+                        }
+                    case 10:
+                        {
+                            rest[1]++;
+                            if (rest[0] <= 0)
+                            {
+                                return false;
+                            }
+                            rest[0]--;
+                            break;
+                        }
+                    case 20:
+                        {
+                            //   rest[2]++;
+                            if (rest[1] > 0 && rest[0] > 0)
+                            {
+                                rest[1]--;
+                                rest[0]--;
+                            }
+                            else if (rest[0] >= 3)
+                            {
+                                rest[0] -= 3;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+
+                            break;
+                        }
+                    default: break;
+                }
+            }
+            return true;
+        }
+
+
+
+        public bool LemonadeChange2(int[] bills)
+        {
+            int five = 0, ten = 0;
+            foreach (int i in bills)
+            {
+                if (i == 5) five++;
+                else if (i == 10) { five--; ten++; }
+                else if (ten > 0) { ten--; five--; }
+                else five -= 3;
+                if (five < 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Leetcode/BinarySearch/[868] Binary Gap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class _868__Binary_Gap
+    {
+        public int BinaryGap(int N)
+        {
+            int last = -1, ans = 0;
+            for (int i = 0; i < 32; ++i)
+                if (((N >> i) & 1) > 0)
+                {
+                    if (last >= 0)
+                        ans = Math.Max(ans, i - last);
+                    last = i;
+                }
+
+            return ans;
+        }
+    }
+}
