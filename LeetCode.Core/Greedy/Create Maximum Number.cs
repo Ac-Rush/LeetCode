4a2fb62738638030da9d0b21f52d8978df009ab6
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-09-06 16:15:00 -0700

    Create_Maximum_Number
    Create_Maximum_Number

---
+++ LeetCode.Core/Greedy/Create Maximum Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.Greedy
+{
+    class Create_Maximum_Number
+    {
+        public int[] MaxNumber(int[] nums1, int[] nums2, int k)
+        {
+            return default;
+        }
+    }
+}
