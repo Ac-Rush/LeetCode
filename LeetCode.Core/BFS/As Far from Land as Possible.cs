a54f780f845612c47d11111a905a9c5232a45040
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-09-13 14:10:50 -0700

    As_Far_from_Land_as_Possible
    As_Far_from_Land_as_Possible

---
+++ LeetCode.Core/BFS/As Far from Land as Possible.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.BFS
+{
+    public class As_Far_from_Land_as_Possible
+    {
+        public int MaxDistance(int[][] grid)
+        {
+            int m = grid.Length, n = grid[0].Length;
+            var queue = new Queue<int[]>();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        queue.Enqueue(new[] { i, j });
+                    }
+                }
+            }
+            var step = 0;
+            var dirs = new int[][] { new[] { 1, 0 }, new[] { -1, 0 }, new[] { 0, 1 }, new[] { 0, -1 } };
+            while (queue.Count > 0)
+            {
+                var count = queue.Count;
+                while (count-- > 0)
+                {
+                    var p = queue.Dequeue();
+                    foreach (var dir in dirs)
+                    {
+                        var i = p[0] + dir[0];
+                        var j = p[1] + dir[1];
+                        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 1) continue;
+                        queue.Enqueue(new[] { i, j });
+                        grid[i][j] = 1;
+                    }
+                }
+                step++;
+            }
+            return step;
+        }
+    }
+}
--- LeetCode.Core/Program.cs
+++ LeetCode.Core/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Leetcode.Dynamic_Programming;
+using LeetCode.Core.BFS;
 using System;
 
 namespace LeetCode.Core
@@ -7,12 +8,22 @@ namespace LeetCode.Core
     {
         static void Main(string[] args)
         {
-            var array = new int[] { 1,3,4,5,6,6,6,6,6,6,6,78};
-           
-            var c = Array.LastIndexOf(array, 6);
-            var d = Array.IndexOf(array,6);
+            TestMetrix();
             Console.Read();
             // Wildcard_Matching_memo.IsMatch("mississippi", "m??*ss*?i*pi");
+        }
+        void TestArray()
+        {
+            var array = new int[] { 1, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 78 };
+
+            var c = Array.LastIndexOf(array, 6);
+            var d = Array.IndexOf(array, 6);
+        }
+
+        static void TestMetrix()
+        {
+            var a = new As_Far_from_Land_as_Possible();
+            a.MaxDistance(new int[][]{ new [] { 1,0,0}, new[] { 0, 0, 0 }, new[] { 0, 0, 0 } });
         }
     }
 }
