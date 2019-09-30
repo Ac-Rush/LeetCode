e1cbfaf4a2931b29159a41c84a8bbb47f569a62b
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-09-30 10:28:58 -0700

    FindKth
    FindKth

---
+++ LeetCode.Core/Lib/Search/FindKth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.Lib.Search
+{
+    public class FindKth
+    {
+        /// <summary>
+        /// Find Kth in sorted array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int FindK(int[] array, int k)
+        {
+            return 0;
+        }
+
+    }
+}
