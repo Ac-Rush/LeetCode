0a742959c649f24f617b7e4082321504a2fa5fd3
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-10-09 17:17:18 -0700

    区别就是一个用了队列 一个用了栈
    区别就是一个用了队列 一个用了栈

--- LeetCode.Core/Lib/Graph/BFS_c.cs
+++ LeetCode.Core/Lib/Graph/BFS_c.cs
@@ -7,6 +7,13 @@ namespace LeetCode.Core.Lib.Graph
 {
     class BFS_c
     {
+        /// <summary>
+        /// <see cref="DFS_C"/> 区别就是一个用了队列 一个用了栈
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="graph"></param>
+        /// <param name="start"></param>
+        /// <returns></returns>
         public HashSet<T> BFS<T>(Graph<T> graph, T start)
         {
             var visited = new HashSet<T>();
