f89774965d00db3be920c7e00ab4b177420a2e76
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-12-17 16:24:45 -0800

    OrderList
    OrderList

--- LeetCode.Core/BinarySearch/Sliding Window Median.cs
+++ LeetCode.Core/BinarySearch/Sliding Window Median.cs
@@ -10,32 +10,31 @@ namespace Leetcode.BinarySearch
     {
         public double[] MedianSlidingWindow(int[] nums, int k)
         {
-            List<double> llist = new List<double>();
+            List<double> llist = new List<double>(); // 存放结果
 
-            if (nums != null && nums.Length > 0 && k > 0)
+            if (nums == null || nums.Length <= 0 || k <= 0) return llist.ToArray<double>(); // validation
+
+            int liPos1 = (k >> 1);  // 第一个数， 右边的
+            int liPos2 = liPos1 + (k & 1) - 1; //第二个数， 左边的
+
+            List<double> llistSlid = new List<double>(); // k的数组
+
+            for (int i = 0; i < nums.Length; ++i)
             {
-                int liPos1 = (k >> 1);
-                int liPos2 = liPos1 + (k & 1) - 1;
-
-                List<double> llistSlid = new List<double>();
-
-                for (int i = 0; i < nums.Length; ++i)
+                if (i >= k)
                 {
-                    if (i >= k)
-                    {
-                        llistSlid.Remove(nums[i - k]);
-                    }
-
-                    int liIndex = llistSlid.BinarySearch(nums[i]);
-                    if (liIndex < 0)
-                    {
-                        liIndex = ~liIndex;
-                    }
-                    llistSlid.Insert(liIndex, nums[i]);
-
-                    if (i >= k - 1)
-                        llist.Add(liPos1 == liPos2 ? llistSlid[liPos1] : ((llistSlid[liPos1] + llistSlid[liPos2]) / 2));
+                    llistSlid.Remove(nums[i - k]);
                 }
+
+                int liIndex = llistSlid.BinarySearch(nums[i]);  //二分查找
+                if (liIndex < 0)
+                {
+                    liIndex = ~liIndex;  //  liIndex = -liIndex - 1;
+                }
+                llistSlid.Insert(liIndex, nums[i]); // 有序插入
+
+                if (i >= k - 1)
+                    llist.Add(liPos1 == liPos2 ? llistSlid[liPos1] : ((llistSlid[liPos1] + llistSlid[liPos2]) / 2));
             }
 
             return llist.ToArray<double>();
---
+++ LeetCode.Core/模板/Fragments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.模板
+{
+    class Fragments
+    {
+        private void OrderList()
+        {
+            var num = 0;
+            List<double> llistSlid = new List<double>(); // k的数组
+            int liIndex = llistSlid.BinarySearch(num);  //二分查找
+            if (liIndex < 0)
+            {
+                liIndex = ~liIndex;  //  liIndex = -liIndex - 1;
+            }
+            llistSlid.Insert(liIndex,num); // 有序插入
+        }
+    }
+}
