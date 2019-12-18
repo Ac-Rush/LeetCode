02912bd496e584b66fe0362dd53279887027a0dd
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2019-12-18 15:55:53 -0800

    Cut_Off_Trees_for_Golf_Event
    Cut_Off_Trees_for_Golf_Event

---
+++ LeetCode.Core/BFS/Cut Off Trees for Golf Event.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Core.BFS
+{
+    class Cut_Off_Trees_for_Golf_Event
+    {
+        int[] dr = { -1, 1, 0, 0 };
+        int[] dc = { 0, 0, -1, 1 };
+        public int CutOffTree(IList<IList<int>> forest)
+        {
+            List<int[]> trees = new List<int[]>();
+            for (int r = 0; r < forest.Count; ++r)
+            {
+                for (int c = 0; c < forest[r].Count; ++c)
+                {
+                    int v = forest[r][c];
+                    if (v > 1) trees.Add(new int[] { v, r, c });  //value ,行，列
+                }
+            }
+            trees.Sort(Comparer<int[]>.Create((a, b) => a[0].CompareTo(b[0])));
+            int ans = 0, sr = 0, sc = 0;
+            foreach (int[] tree in trees)
+            {
+                int d = bfs(forest, sr, sc, tree[1], tree[2]);
+                if (d < 0) return -1; // 没有结果
+                ans += d;
+                sr = tree[1]; sc = tree[2];
+            }
+            return ans;
+        }
+
+        public int bfs(IList<IList<int>> forest, int sr, int sc, int tr, int tc)
+        {
+            int R = forest.Count, C = forest[0].Count;
+            var queue = new Queue<int[]>();
+            queue.Enqueue(new int[] { sr, sc, 0 }); // 入 当前节点
+            var seen = new bool[R,C];
+            seen[sr,sc] = true;
+            while (queue.Any())
+            {
+                int[] cur = queue.Dequeue();
+                if (cur[0] == tr && cur[1] == tc) return cur[2]; //结束条件
+                for (int di = 0; di < 4; ++di)
+                {
+                    int r = cur[0] + dr[di];
+                    int c = cur[1] + dc[di];
+                    if (0 <= r && r < R && 0 <= c && c < C &&
+                        !seen[r,c] && forest[r][c] > 0) // validate 
+                    {
+                        seen[r,c] = true;
+                        queue.Enqueue(new int[] { r, c, cur[2] + 1 });
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
--- LeetCode.Core/BinarySearch/Swim in Rising Water.cs
+++ LeetCode.Core/BinarySearch/Swim in Rising Water.cs
@@ -3,43 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lucene.Net.Util;
 
 namespace Leetcode.BinarySearch
 {
-    class Swim_in_Rising_Water
+    class Swim_in_Rising_Water_PQ
     {
-         static int[][] steps = new int[][] {new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 }};
         /// <summary>
-        /// DP + DFS
+        /// pq 解法，
         /// </summary>
         /// <param name="grid"></param>
         /// <returns></returns>
-        public int SwimInWater(int[,] grid)
+        public int swimInWater(int[][] grid)
         {
-            var height = grid.GetLength(0);
-            var weight = grid.GetLength(1);
-            var max = new int[height, weight] ;
-            for (int index00 = 0; index00 < max.GetLength(0); index00++)
-                for (int index01 = 0; index01 < max.GetLength(1); index01++)
+            int n = grid.Length;
+            SortedSet<Tuple<int, int, int>> pq = new SortedSet<Tuple<int, int, int>>(); // tuple 自动实现 compare
+            var visited = new bool[n,n];
+            int[][] steps = new int[][] { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
+
+            visited[0,0] = true;
+            pq.Add(new Tuple<int, int, int>(grid[0][0], 0, 0 ));
+            while (pq.Any())
+            {
+                var info = pq.Min;
+                pq.Remove(info);
+                int i = info.Item2, j = info.Item3, max = info.Item1;
+                foreach (int[] dir in steps)
                 {
-                    max[index00, index01] = int.MaxValue;
+                    int newI = dir[0] + i, newJ = dir[1] + j;
+                    if (newI < 0 || newI >= n || newJ < 0 || newJ >= n) continue;
+                    if (!visited[newI,newJ])
+                    {
+                        visited[newI,newJ] = true;
+                        int newmax = Math.Max(max, grid[newI][newJ]);
+                        if (newI == n - 1 && newJ == n - 1) return newmax;
+                        pq.Add(new Tuple<int, int, int>(newmax, newI, newJ) );
+                    }
                 }
-            dfs(grid, max, 0, 0, grid[0,0]);
+            }
+
+            return 0;
+        }
+    }
+
+    class Swim_in_Rising_Water
+    {
+
+
+
+
+
+
+
+         static int[][] steps = new int[][] {new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 }};
+        /// <summary>
+        /// DFS 
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int SwimInWater(int[][] grid)
+        {
+            var height = grid.Length;
+            var weight = grid[0].Length;
+            var max = new int[height, weight];
+            for (int index00 = 0; index00 < height; index00++)
+            for (int index01 = 0; index01 < weight; index01++)
+            {
+                max[index00, index01] = int.MaxValue;
+            }
+            dfs(grid, max, 0, 0, grid[0][0]);
             return max[height - 1, weight - 1];
         }
 
-        private void dfs(int[,] grid, int[,] max, int x, int y, int cur)
+        private void dfs(int[][] grid, int[,] max, int x, int y, int cur)
         {
-            int n = grid.GetLength(0);
-            if (x < 0 || x >= n || y < 0 || y >= n || Math.Max(cur, grid[x,y]) >= max[x,y])
+            int n = grid.Length;
+            if (x < 0 || x >= n || y < 0 || y >= n || Math.Max(cur, grid[x][y]) >= max[x, y]) // validate check
                 return;
-            max[x,y] = Math.Max(cur, grid[x,y]);
+            max[x, y] = Math.Max(cur, grid[x][y]);
             foreach (var s in steps)
             {
-                dfs(grid, max, x + s[0], y + s[1], max[x,y]);
+                dfs(grid, max, x + s[0], y + s[1], max[x, y]);
             }
         }
-
-        
     }
 }
