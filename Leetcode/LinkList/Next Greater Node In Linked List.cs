f6d5df36248cb487c1401f9657464abc8a887e32
Ac-Rush <justboy1987@gmail.com>
2019-12-30 20:08:55 -0800

    //递减单调栈， 用于求第一个比自己大的
    //递减单调栈， 用于求第一个比自己大的

---
+++ Leetcode/LinkList/Next Greater Node In Linked List.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.LinkList
+{
+    class Next_Greater_Node_In_Linked_List
+    {
+        public int[] NextLargerNodes(ListNode head)
+        {
+            var values = new List<int>();
+            while (head != null)
+            {
+                values.Add(head.val);
+                head = head.next;
+            }
+
+            var result = new int[values.Count];
+            var stack = new Stack<int>();
+
+            //递减单调栈， 用于求第一个比自己大的 
+            for (var i = 0; i < values.Count; i++)
+            {
+                //
+                while (stack.Any() && values[stack.Peek()] < values[i])
+                    result[stack.Pop()] = values[i];
+                stack.Push(i);
+            }
+            return result;
+
+        }
+    }
+}
