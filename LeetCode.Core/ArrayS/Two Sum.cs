72d1a028316b045789f2a1162c37c1728fb9391b
Ac-Rush <37987464+Ac-Rush@users.noreply.github.com>
2020-01-03 15:23:49 -0800

    Two_Sum
    Two_Sum

---
+++ LeetCode.Core/ArrayS/Two Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.ArrayS
+{
+    class Two_Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int,int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new[] {i, dict[target - nums[i]]};
+                }
+
+                dict[nums[i]] = i;
+            }
+
+            return new[] {-1, -1};
+        }
+    }
+}
